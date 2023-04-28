282de5a8d3795558eadd2dbb2e279c47bcc4d91a
deeksha-pandey-cedcoss <126545590+deeksha-pandey-cedcoss@users.noreply.github.com>
2023-04-28 15:49:54 +0530

    Add files via upload
    part-2 done

---
+++ phalcon-events-part1/phalcon-app/src/public/index.php
@@ -0,0 +1,89 @@
+<?php
+
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Url;
+use Phalcon\Db\Adapter\Pdo\Mysql;
+use Phalcon\Config;
+use Phalcon\Escaper;
+
+$config = new Config([]);
+
+// Define some absolute path constants to aid in locating resources
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+// Register an autoloader
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        APP_PATH . "/controllers/",
+        APP_PATH . "/models/",
+    ]
+);
+$loader->registerNamespaces(
+    [
+        'MyApp\handle' => APP_PATH . '/handler/'
+    ]
+);
+$loader->registerClasses(
+    [
+        'Listner'   => APP_PATH . '/handler/Listner.php',
+    ]
+);
+
+$loader->register();
+
+$container = new FactoryDefault();
+
+$container->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+        return $view;
+    }
+);
+
+$container->set(
+    'url',
+    function () {
+        $url = new Url();
+        $url->setBaseUri('/');
+        return $url;
+    }
+);
+$container->set(
+    'escaper',
+    function () {
+        return new Escaper();
+    }
+);
+
+$container->set(
+    'db',
+    function () {
+        return new Mysql(
+            [
+                'host'     => 'mysql-server',
+                'username' => 'root',
+                'password' => 'secret',
+                'dbname'   => 'newdb',
+            ]
+        );
+    }
+);
+$application = new Application($container);
+try {
+    // Handle the request
+    $response = $application->handle(
+        $_SERVER["REQUEST_URI"]
+    );
+
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
