a7ade407523d138d0f322bc8a08c829e8dee40b8
deeksha01032023 <deekshapandey@cedcoss.com>
2023-05-01 09:38:56 +0530

    done word

--- src/app/handler/Listner.php
+++ src/app/handler/Listner.php
@@ -9,17 +9,14 @@ class Listner extends Injectable
     {
         $di = $this->getDI();
         $connection = $di->get('db');
-        $product = $this->db->fetchAll(
-            "SELECT * FROM settings WHERE `id`=1",
-            \Phalcon\Db\Enum::FETCH_ASSOC
-        );
+        $product = $this->db->fetchAll("SELECT * FROM settings WHERE `id`=1", \Phalcon\Db\Enum::FETCH_ASSOC);
         if ($product[0]['title'] == "tag1") {
             $_POST['pname'] = $_POST['pname'] . "-" . $_POST['ptags'];
         }
-        if ($_POST['pprice'] == "") {
+        if ($_POST['pprice'] == "" || $_POST['pprice'] == 0) {
             $_POST['pprice'] = $product[0]['price'];
         }
-        if ($_POST['pstock'] == "") {
+        if ($_POST['pstock'] == "" || $_POST['pstock'] == 0) {
             $_POST['pstock'] = $product[0]['stock'];
         }
     }
@@ -27,10 +24,7 @@ class Listner extends Injectable
     {
         $di = $this->getDI();
         $connection = $di->get('db');
-        $product = $this->db->fetchAll(
-            "SELECT * FROM settings WHERE `id`=1",
-            \Phalcon\Db\Enum::FETCH_ASSOC
-        );
+        $product = $this->db->fetchAll("SELECT * FROM settings WHERE `id`=1", \Phalcon\Db\Enum::FETCH_ASSOC);
 
         if ($_POST['zipcode'] == "") {
             $_POST['zipcode'] = $product[0]['zipcode'];
